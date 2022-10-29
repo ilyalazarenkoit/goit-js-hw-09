9a4c7412b238a80565a26f8af0cd538b7ab7f2e9
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2022-10-29 19:12:04 +0300

    test commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+
+
+let date = new Date()
+
+date.toString()
+console.log(date)
+
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getFullYear());
+console.log(); 
