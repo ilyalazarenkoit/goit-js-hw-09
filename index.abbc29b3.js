33d9b395978a70ee07e61c2e5f91486237772db3
ilyalazarenkoit <112101017+ilyalazarenkoit@users.noreply.github.com>
2022-10-29 16:16:41 +0000

    Deploying to gh-pages from @ ilyalazarenkoit/goit-js-hw-09@9a4c7412b238a80565a26f8af0cd538b7ab7f2e9 🚀

---
+++ index.abbc29b3.js
@@ -0,0 +1,2 @@
+!function(){console.log("hello world");var o=new Date;o.toString(),console.log(o),console.log(o.getDate()),console.log(o.getDay()),console.log(o.getFullYear()),console.log()}();
+//# sourceMappingURL=index.abbc29b3.js.map
